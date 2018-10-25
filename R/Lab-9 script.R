56781a8691670036b75f0aacc4f648dafcf4894b
Obenland <obenlan2@illinois.edu>
2018-10-25 10:33:18 -0500

    testing package installation

--- R/Lab-9 script.R
+++ R/Lab-9 script.R
@@ -5,3 +5,8 @@ SNPmat <- matrix(c(2,4,6,8,10,12,14,16,18,3,6,9,12,15,18), nrow = 5, ncol = 3)
 minorAlleleFrequency(SNPmat)
 filterSNPs(SNPmat, minMAF = 0.05)
 mafHist(SNPmat)
+
+install.packages("SNPfreq_0.0.tar.gz", repos = NULL)
+library(SNPfreq)
+?mafHist
+getwd()
