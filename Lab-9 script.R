31ad220a20528dea6f05b39a82eb6e4f39dd9a83
Obenland <obenlan2@illinois.edu>
2018-10-25 09:49:46 -0500

    added information for .RD file

--- Lab-9 script.R
+++ Lab-9 script.R
@@ -1,2 +1,7 @@
 source('SNPfreq_functions.R')
-prompt(minorAlleleFrequency)
+prompt(minorAlleleFrequency) #makes .RD file
+
+SNPmat <- matrix(c(2,4,6,8,10,12,14,16,18,3,6,9,12,15,18), nrow = 5, ncol = 3)
+minorAlleleFrequency(SNPmat)
+filterSNPs(SNPmat, minMAF = 0.05)
+mafHist(SNPmat)
